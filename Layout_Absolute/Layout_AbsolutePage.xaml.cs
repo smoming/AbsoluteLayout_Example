c27e589ad31beeb77e3a208a67ee6351852064dd
smoming <smoming1031@gmail.com>
2017-04-08 00:43:03 +0800

    AbsoluteLayout_Example
    AbsoluteLayout Example

---
+++ Layout_Absolute/Layout_AbsolutePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+using System;
+using System.Collections.Generic;
+
+namespace Layout_Absolute
+{
+	public partial class Layout_AbsolutePage : ContentPage
+	{
+
+		//int sleepSeconds = 0;
+		//int waitSecond = 5;
+		public Layout_AbsolutePage()
+		{
+			InitializeComponent();
+
+			//Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+			//{
+			//	sleepSeconds++;
+			//	if (sleepSeconds >= waitSecond)
+			//	{
+			//		xBoxView.IsVisible = false;
+			//		xActivityIndicator.IsRunning = false;
+			//	}
+
+			//	return sleepSeconds < waitSecond;
+			//});
+		}
+	}
+}
